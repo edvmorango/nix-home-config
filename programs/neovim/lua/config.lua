641302c926be5c6fe07bfddf01264d1586bc4c12
J. Eduardo V. Morango <edvmorango@gmail.com>
2022-10-17 15:22:26 -0300

    draft: neotest

--- programs/neovim/lua/config.lua
+++ programs/neovim/lua/config.lua
@@ -1,6 +1,7 @@
 -- This file will become init.lua and replace init.vim when home-manager starts to support this approach
 require('basic')
 require('completion')
+require('neotest')
 require('haskell')
 require('json')
 require('keys')
---
+++ programs/neovim/lua/neotest.lua
@@ -0,0 +1,18 @@
+require("neotest").setup({
+  adapters = {
+    require("neotest-scala")({
+      -- Command line arguments for runner
+      -- Can also be a function to return dynamic values
+      args = { "--no-color" },
+      -- Runner to use. Will use bloop by default.
+      -- Can be a function to return dynamic value.
+      -- For backwards compatibility, it also tries to read the vim-test scala config.
+      -- Possibly values bloop|sbt.
+      runner = "bloop",
+      -- Test framework to use. Will use utest by default.
+      -- Can be a function to return dynamic value.
+      -- Possibly values utest|munit|scalatest.
+      framework = "munit",
+    })
+  }
+})
