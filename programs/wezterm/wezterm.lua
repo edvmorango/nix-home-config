d3f262de075ccd3ceac4b106cbce8525599f99af
J. Eduardo V. Morango <edvmorango@gmail.com>
2022-11-20 17:44:04 -0300

    feat: mouse selection copy

--- programs/wezterm/wezterm.lua
+++ programs/wezterm/wezterm.lua
@@ -16,5 +16,17 @@ return {
       action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
     }
   },
-  warn_about_missing_glyphs = false
+  warn_about_missing_glyphs = false,
+
+  mouse_bindings = {
+    -- Change the default click behavior so that it only selects
+    -- text and doesn't open hyperlinks, and that it populates
+    -- the Clipboard rather the PrimarySelection which is part
+    -- of the default assignment for a left mouse click.
+    {
+      event = { Up = { streak = 1, button = 'Left' } },
+      mods = 'NONE',
+      action = wezterm.action.CompleteSelection 'Clipboard',
+    },
+  },
 }
